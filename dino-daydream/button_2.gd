51a31aa1286bb0d2f7984901ee1ec7622da49f7e
Addy <adelaide.bornn@gmail.com>
2025-09-28 17:46:21 -0700

    title

--- dino-daydream/button_2.gd
+++ dino-daydream/button_2.gd
@@ -3,11 +3,12 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label2.visible = false
 
+var has_button_been_pressed = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 func _on_button_pressed():
-	$Label2
+	$Label2.visible = true
